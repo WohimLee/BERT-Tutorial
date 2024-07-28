df3d6553411dab4bc0d0a6c15b41fc1b4c316bf2
AZen <418130444@qq.com>
2024-07-28 17:58:24 +0800

    update

--- test.py
+++ test.py
@@ -1,3 +1,8 @@
 
+from transformers import GPT2Tokenizer, GPT2Model
 
-from transformers import BertModel
+# BertTokenizer: word2index; BertModel: word2vec
+from transformers import BertTokenizer, BertModel
+
+from transformers import AutoModel, AutoTokenizer
+
